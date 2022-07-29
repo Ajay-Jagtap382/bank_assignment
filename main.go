806a8bf2f5ce37849dd3b25df02797b65519336c
Ajay-Jagtap382 <ajayjagtap382@gmail.com>
2022-07-29 15:40:21 +0530

    choosing role function is written

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func start() {
+
+	fmt.Println("Enter the number : ")
+	choice := -1
+	fmt.Scanln(&choice)
+
+	if choice == 1 {
+
+	} else if choice == 2 {
+
+	} else {
+		fmt.Println("Incorrect Input!! Try again!!")
+		start()
+	}
+}
+
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello , Enter the number which suits your role ")
+	fmt.Println("1 .Accountant ")
+	fmt.Println("2 .Customer ")
+
 }
